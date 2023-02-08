0a4b28faa9ab42b7c80bf5ef5a0fde25e160c13a
aleino0 <113008053+aleino0@users.noreply.github.com>
2023-02-08 09:24:57 +0100

    Add files via upload

---
+++ Kolekcije/Kolekcije/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolekcije
+{
+
+    class Program
+    {
+        class Klasa
+        {
+            int godina;
+            string prezime;
+
+            public Klasa(int godina, string prezime)
+            {
+                this.godina = godina;
+                this.prezime = prezime;
+            }
+
+            public override string ToString()
+            {
+                string ispis = "Godina: " + this.godina +
+                    " Prezime: " + this.prezime;
+                return ispis;
+            }
+
+            public int Godina { get => godina; set => godina = value; }
+        }
+
+        static void Main(string[] args)
+        {
+            //definiranje ArrayList kolekcije
+            ArrayList al = new ArrayList();
+
+            int broj = 1;
+            string ime = "ime";
+            bool provjera = false;
+            Klasa objekt = new Klasa(2023, "prezime");
+
+            al.Add(objekt);
+            al.Add(provjera);
+            al.Add(broj);
+            al.Add(ime);
+
+            //ispis
+            Console.WriteLine("------ispis svih elemenata------");
+            for(int i =0;i<al.Count;i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+            al.RemoveAt(2);
+            
+
+            Console.WriteLine("------ispis svih elemenata------");
+            for (int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+            al.Insert(2, broj);
+
+            Console.WriteLine("------ispis svih elemenata------");
+            for (int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ List/List/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        class Klasa
+        {
+            int godina;
+            string ime, prezime;
+
+            public Klasa(int godina, string prezime)
+            {
+                this.godina = godina;
+                this.prezime = prezime;
+               
+            }
+
+            public override string ToString()
+            {
+                string ispis = "Godina: " + this.godina +
+                    "Ime: " + this.ime +
+                    " Prezime: " + this.prezime;
+                return ispis;
+            }
+
+            public int Godina { get => godina; set => godina = value; }
+            public string Prezime { get => prezime; set => prezime = value; }
+            public string Ime { get => ime; set => ime = value; }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Klasa> ListaObjekata = new List<Klasa>();
+            int godina;
+            string prezime;
+               
+            for(int i =0;i<3;i++)
+            {
+                Console.WriteLine("Unesi godinu: ");
+                godina = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Unesi prezime: ");
+                prezime = Console.ReadLine();
+                Klasa objekt = new Klasa(godina, prezime);
+
+                ListaObjekata.Add(objekt);
+            }
+
+            foreach(Klasa o in ListaObjekata)
+            {
+                if(o.Godina<2023)
+                {
+                    o.Ime = "Gabriel";
+                    Console.WriteLine(o.Prezime);
+                    Console.WriteLine(o.Ime);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
